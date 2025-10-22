a4a081f476ab89f2f5bca4530e1236212feba3a0
kammazu12 <133350949+kammazu12@users.noreply.github.com>
2025-10-22 15:53:25 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,34 @@
 from extensions import *
 from sqlalchemy.dialects.postgresql import TSVECTOR
+import os
 
 class City(db.Model):
     __tablename__ = "city"
 
     id = db.Column(db.Integer, primary_key=True)
     city_name = db.Column(db.String(100), nullable=False)
+    ascii_name = db.Column(db.String(100), nullable=False)
+    admin1 = db.Column(db.String(100))
+    admin2 = db.Column(db.String(100))
+    admin3 = db.Column(db.String(100))
+    admin4 = db.Column(db.String(100))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     zipcode = db.Column(db.String(20))  # string legyen
     country_code = db.Column(db.String(2), db.ForeignKey('countries.code'))
-    search_vector = db.Column(TSVECTOR)
+    if os.environ.get('DB_TYPE') == 'postgres':
+        search_vector = db.Column(TSVECTOR)
+
+    if os.environ.get("DB_TYPE") == "postgres":
+        score_expr = func.greatest(
+            func.similarity(City.city_name, term),
+            func.similarity(City.ascii_name, term),
+            func.similarity(City.admin1, term),
+            func.similarity(City.admin2, term),
+            func.similarity(City.admin3, term)
+        )
+    else:
+        score_expr = func.length(func.replace(City.city_name, term, term))  # nagyon egyszerű mérőszám SQLite-on
 
 
 class CityZipcode(db.Model):
@@ -29,7 +47,8 @@ class AlterName(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
     alternames = db.Column(db.String(1000), nullable=False)
-    search_vector = db.Column(TSVECTOR)
+    if os.environ.get('DB_TYPE') == 'postgres':
+        search_vector = db.Column(TSVECTOR)
 
 
 class Country(db.Model):
@@ -47,4 +66,3 @@ class Country(db.Model):
     iso_code = db.Column(db.String(3), nullable=False)
 
     cities = db.relationship('City', backref='country', lazy=True, primaryjoin="Country.code==foreign(City.country_code)")
-
